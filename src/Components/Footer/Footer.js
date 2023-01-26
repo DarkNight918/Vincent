353d1797615e0830826630cf6e7c3c20e3f31c94
Dark Night <darknight.dev123@gmail.com>
2023-01-26 08:19:49 -0500

    Last

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -59,7 +59,7 @@ const Footer = () => {
 
             <div className="contact-icon d-flex align-items-center mb-4">
               <FontAwesomeIcon icon={faHome} />
-              <span> 11942, Hillbrook Dr, Houston, Texas, US</span>
+              <span> Western District, Hong Kong</span>
             </div>
 
             <div className="contact-icon d-flex align-items-center mb-4">
